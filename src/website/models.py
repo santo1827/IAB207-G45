f32596eef9f53a3ef6411673eba31c84d573bb7e
4tiernan <41096273+4tiernan@users.noreply.github.com>
2025-09-18 15:56:32 +1000

    Added basic login functionality from provided templates

--- src/website/models.py
+++ src/website/models.py
@@ -3,9 +3,48 @@ from datetime import datetime
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users' # good practice to specify table name
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    emailid = db.Column(db.String(100), index=True, nullable=False)
+	#password is never stored in the DB, an encrypted password is stored
+	# the storage should be at least 255 chars long
+    password_hash = db.Column(db.String(255), nullable=False)
+    # relation to call user.comments and comment.name
+    comments = db.relationship('Comment', backref='user')
+
+    # string print method
+    def __repr__(self):
+        return f"Name: {self.name}"
 
 class Event(db.Model):
+    __tablename__ = 'destinations'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.String(200))
+    image = db.Column(db.String(400))
+    currency = db.Column(db.String(3))
+    # ... Create the Comments db.relationship
+	# relation to call destination.comments and comment.destination
+    comments = db.relationship('Comment', backref='destination')
+
+	# string print method
+    def __repr__(self):
+        return f"Name: {self.name}"
 
 class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(400))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    # add the foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    destination_id = db.Column(db.Integer, db.ForeignKey('destinations.id'))
 
-class Order(db.Model):
+    # string print method
+    def __repr__(self):
+        return f"Comment: {self.text}"
+
+
+#class Order(db.Model):
+#    a=0
